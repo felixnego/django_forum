1ef19786cd41eb54307412bbb2af416dc3386050
felixnego <felixnego@gmail.com>
2019-10-11 12:44:07 +0300

    added post views and urls

--- forum/groups/urls.py
+++ forum/groups/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
 from groups import views 
 
-app_name = 'group'
+app_name = 'groups'
 
-url_patterns = [
+urlpatterns = [
     path('', views.ListGroups.as_view(), name='all'),
     path('new/', views.CreateGroups.as_view(), name='create'),
     re_path(r'posts/in/(?P<slug>[-\w]+/$)', views.SingleGroups.as_view(), name='single')
